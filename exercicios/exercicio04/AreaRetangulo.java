dfa96ecfa8993cf3f4277da7c4f9e5f74e6515a2
Alexandre <xandeferreiradasilva@gmail.com>
2026-08-26 14:18:00 +0000

    area retangulo

---
+++ exercicios/exercicio04/AreaRetangulo.java
@@ -0,0 +1,31 @@
+package exercicios.exercicio04;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+    /* Enunciado: Faça um programa que leia a base e a altura de um retângulo. 
+    Em seguida, calcule e mostre:
+    
+    A área (base * altura)
+    
+    O perímetro (2 * (base + altura))
+    
+    Conceitos: Atribuição de múltiplas saídas a partir dos mesmos dados de entrada.
+    
+    Exemplo de Entrada: Base: 5.0, Altura: 3.0 
+    Saída esperada: Área: 15.0, Perímetro: 16.0
+
+    */
+
+    public class AreaRetangulo{
+        public static void main(String[] args){
+
+            Scanner sc = new Scanner(System.in);
+            DecimalFormat df = new DecimalFormat("#.00");
+
+
+
+        }
+
+
+    }
