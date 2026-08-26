1f392ceb6d26fe4caee25c880d0c64a7f35906bf
alexandrefrrs <xandeferreiradasilva@gmail.com>
2026-08-25 23:37:41 -0300

    ConversorTemp

---
+++ exercicios/exercicio02/ConversorTemp.java
@@ -0,0 +1,21 @@
+package pob3.exercicios.Exercicio02;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+    /*Enunciado: Crie um programa que solicite uma temperatura em graus Celsius ($C$) e a converta para Fahrenheit (F) utilizando a fórmula:
+    
+    F = (C * 1.8) + 32 
+
+    Conceitos: Operações aritméticas com precedência de parênteses e formatação de saída.
+    
+    Exemplo de Entrada: 25.0  Saída esperada: 77    */
+
+    public class ConversorTemp{
+        public static void main(String[] args){
+
+            Scanner sc = new Scanner(System.in);
+            DecimalFormat df = new DecimalFormat("#.00");
+        }
+
+    }
