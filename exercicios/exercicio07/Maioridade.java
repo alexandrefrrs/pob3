8a3b2e708b9babc0af454166ee18917ca46108cf
alexandrefrrs <xandeferreiradasilva@gmail.com>
2026-09-02 20:34:29 -0300

    maioridade

---
+++ exercicios/exercicio07/Maioridade.java
@@ -0,0 +1,19 @@
+package exercicios.exercicio07;
+
+import java.util.Scanner;
+
+    /*Enunciado: Crie um programa que leia o ano de nascimento de uma pessoa e o ano atual. 
+    Calcule a idade e exiba se ela já é maior de idade (18 anos ou mais) ou menor de idade.
+
+    Conceitos: Operadores relacionais (>=) e subtração básica combinada com decisão.
+     */
+
+    public class Maioridade{
+        public static void main(string[] args){
+
+            Scanner sc = new Scanner(System.in);
+
+
+            
+        }
+    }
